2f2834a3c6cbb4a270173c77ab69e2b607d08dd5
Matt Moore <mattmoore@pop-os.localdomain>
2020-05-31 17:29:53 -0500

    search service event emitted and Index subscribed to it

--- Services/BeerSearchService.cs
+++ Services/BeerSearchService.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System;
 using BeerApplication.Models;
 
 namespace BeerApplication.Services  
@@ -17,22 +18,14 @@ namespace BeerApplication.Services
         {
             _httpClient = httpClient;
             _httpClient.BaseAddress = new System.Uri(_apiRoot);
+
         }
 
-        // TODO: Learn about event/Action
-        //       https://docs.microsoft.com/en-us/dotnet/api/system.action?view=netcore-3.1
-
-        // TODO: Learn about async/await/Task
-        //       https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/async/
-        //       https://docs.microsoft.com/en-us/dotnet/standard/parallel-programming/task-based-asynchronous-programming
+        public event Action<Beer[]> SearchCompleted;
         public async Task<Beer[]> DoSearchAsync(string searchString)
         {
-            // TODO: Emit SearchCompletedEvent when done
             beers = await _httpClient.GetFromJsonAsync<Beer[]>($"beers?beer_name={searchString}");
-            if (beers.Length > 0)
-            {
-                System.Console.WriteLine(beers[0].Name);
-            }
+            SearchCompleted?.Invoke(beers);
             return beers;
         }
     }
