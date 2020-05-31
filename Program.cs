a9722c668353677687b18c2d2f75892aff006034
Matt Moore <mattmoore@pop-os.localdomain>
2020-05-30 23:32:20 -0500

    more framing for home-rolled modal...last commit before trying to pull in Blazored Modal package

--- Program.cs
+++ Program.cs
@@ -16,8 +16,8 @@ namespace BeerApplication
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddTransient(sp => new Services.BeerSearchService());
+            builder.Services.AddTransient(sp => new Services.ModalService());
             await builder.Build().RunAsync();
         }
     }
